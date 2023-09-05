d56df0dc2ad8d2082b3fe4bff176f3c2152bf697
Skliar Ivan <rebja605@gmail.com>
2023-09-05 10:04:00 +0300

    Update lb22.java

--- LB2/lb22.java
+++ LB2/lb22.java
@@ -3,32 +3,18 @@ import java.util.Scanner;
 public class lb22 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введіть кількість рядків: ");
-        int rows = scanner.nextInt();
-        System.out.print("Введіть кількість стовпців: ");
-        int cols = scanner.nextInt();
-        int[][] my2DArray = create2DArray(rows, cols);
-        print2DArray(my2DArray);
+        System.out.print("Введіть кількість рівнів ялинки: ");
+        int levels = scanner.nextInt();
+        printTree(levels);
     }
 
-    public static int[][] create2DArray(int rows, int cols) {
-        int[][] array = new int[rows][cols];
-        int value = 1;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                array[i][j] = value;
-                value += 3;
+    public static void printTree(int levels) {
+        for (int i = 1; i <= levels; i++) {
+            for (int j = 1; j <= levels - i; j++) {
+                System.out.print(" ");
             }
-        }
-
-        return array;
-    }
-
-    public static void print2DArray(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
             }
             System.out.println();
         }
