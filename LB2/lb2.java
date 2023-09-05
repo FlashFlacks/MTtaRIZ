077564bd5fcbb9ab26d5521a1c8decc092ff215c
Skliar Ivan <144028301+FlashFlacks@users.noreply.github.com>
2023-09-05 09:44:59 +0300

    Update lb2.java

--- LB2/lb2.java
+++ LB2/lb2.java
@@ -1,1 +1,14 @@
-
+public class lb2 {
+public static void main(String[] args) {
+  int heigh = 6;
+  for (int i=1; i<=heigh; i++) {
+    for (int space=1; space<=heigh-i; space++) {
+      System.out.print(" ");
+    }
+    for (int star=1; star<=i*2-1; star++) {
+      System.out.print("*");
+    }
+    System.out.println();
+    }
+  }
+} 
