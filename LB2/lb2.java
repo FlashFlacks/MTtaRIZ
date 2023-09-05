442d2ff7a7aae833c6e0bd5e6bd1ec7151165b48
Skliar Ivan <rebja605@gmail.com>
2023-09-05 10:23:45 +0300

    Update lb2.java

--- LB2/lb2.java
+++ LB2/lb2.java
@@ -1,15 +1,52 @@
 
+import java.util.Scanner;
+
 public class lb2 {
-public static void main(String[] args) {
-  int heigh = 6;
-  for (int i=1; i<=heigh; i++) {
-    for (int space=1; space<=heigh-i; space++) {
-      System.out.print(" ");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Введення кількості рівнів ялинки
+        System.out.print("ввод количество уровней ёлки: ");
+        int treeLevels = scanner.nextInt();
+
+        // Виведення ялинки
+        printTree(treeLevels);
+
+        // Введення розмірів масиву
+        System.out.print("ввод к-ства строк массива: ");
+        int rows = scanner.nextInt();
+        System.out.print("ввод к-ства столбцов массива: ");
+        int cols = scanner.nextInt();
+
+        // Створення та виведення масиву
+        int[][] my2DArray = createAndPrint2DArray(rows, cols);
     }
-    for (int star=1; star<=i*2-1; star++) {
-      System.out.print("*");
+
+    public static void printTree(int levels) {
+        for (int i = 1; i <= levels; i++) {
+            for (int j = 1; j <= levels - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
-    System.out.println();
+
+    public static int[][] createAndPrint2DArray(int rows, int cols) {
+        int[][] array = new int[rows][cols];
+        int value = 1;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                array[i][j] = value;
+                System.out.print(array[i][j] + " "); // Виведення значення
+                value += 3;
+            }
+            System.out.println(); // Перехід на новий рядок
+        }
+
+        return array;
     }
-  }
-} 
+}
