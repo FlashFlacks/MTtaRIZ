4e553d471bc6b4fd09145ca18776e7e25137ac69
Skliar Ivan <rebja605@gmail.com>
2023-09-05 09:06:38 +0300

    Update lb1.java

--- LB1/lb1.java
+++ LB1/lb1.java
@@ -2,7 +2,7 @@ public class lb1 {
         public static void main(String[] args) {
         
         System.out.println("Its works");
-        
+        System.out.println("Its realy works, but im not sure ;p");
         }
         
         }
